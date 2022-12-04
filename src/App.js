3d8b53f239278416975b9f3d72e73a7fe9fc1dc6
Icaro Manuel <icaroomanuel@gmail.com>
2022-12-03 21:49:59 -0300

    atualização

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase  from './components/Frase';
 
 function App() {
   return (
     <div className="App">
-
-      < HelloWorld />
+      <h1> Testando Css   </h1>      
+      < Frase/>
       <SayMyName nome="Icaro" />
       <Pessoa nome="Icaro" idade="17" profissao="programador"  foto='https://via.placeholder.com/150  ' />
     </div>
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,7 +1,10 @@
+import styles from './Frase.module.css'
+
+
 function Frase () {
     return (
-        <div>
-        <p> Este Componente é uma frase! </p>
+        <div className={styles.fraseContainer}>
+        <p className={styles.fraseContent}> Este Componente é uma frase! </p>
     </div>
     )
 }
